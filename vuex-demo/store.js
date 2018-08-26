2591f7e75933bc763ab7390e9993426cbe52b42e
人心思动 <805396056@qq.com>
2018-08-26 16:31:16 +0800

    Update store.js

--- vuex-demo/store.js
+++ vuex-demo/store.js
@@ -1,37 +1,35 @@
-import Vue from "vue"
-import Vuex from "vuex"
-
+import Vue from 'vue'
+import Vuex from 'vuex'
 Vue.use(Vuex)
-
 const store = new Vuex.Store({
-  state: {
-    count: 1
-  },
-  mutations: {
-    add(state, n) {
-      state.count += n;
+    state: {
+        count: 0
     },
-    reduce(state) {
-      state.count--;
-    }
-  },
-//   getters: {
-//     count: state => state.count += 100
-//   },
-  actions: {
-    addAction(context) {
-      context.commit('add', 2);
-      setTimeout(() => {
-        context.commit('reduce')
-      }, 2000);
-      console.log('我比reduce提前执行');
+    mutations: {// 包含了多个直接更新state函数的对象
+        INCREMENT(state) {
+            state.count = state.count + 1;
+        },
+        DECREMENT(state) {
+            state.count = state.count - 1;
+        }
     },
-    reduceAction({
-      commit
-    }) {
-      commit('reduce')
-    }
-  }
-});
+    getters: {   // 当读取属性值时自动调用并返回属性值
+        evenOrOdd(state) {
+            return state.count % 2 === 0 ? "偶数" : "奇数";
+        }
+    },
+    actions: { // 包含了多个对应事件回调函数的对象
+        incrementIfOdd({ commit, state }) { // 带条件的action
+            if (state.count % 2 === 1) {
+                commit('INCREMENT')
+            }
+        },
+        incrementAsync({ commit }) { //异步的action
+            setInterval(() => {
+                commit('INCREMENT')
+            }, 2000);
+        }
 
-export default store;
+    }
+})
+export default store //用export default 封装代码，让外部可以引用
