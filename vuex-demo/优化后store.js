a2f57eacb4db226ae77911b879606811de57b620
人心思动 <805396056@qq.com>
2018-08-26 20:46:42 +0800

    Add files via upload

---
+++ vuex-demo/优化后store.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+const store = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {// 包含了多个直接更新state函数的对象
+        increment(state) {
+            state.count = state.count + 1;
+        },
+        decrement(state) {
+            state.count = state.count - 1;
+        }
+    },
+    getters: {   // 当读取属性值时自动调用并返回属性值
+        evenOrOdd(state) {
+            return state.count % 2 === 0 ? "偶数" : "奇数";
+        }
+    },
+    actions: { // 包含了多个对应事件回调函数的对象
+        incrementIfOdd({ commit, state }) { // 带条件的action
+            if (state.count % 2 === 1) {
+                commit('increment')
+            }
+        },
+        incrementAsync({ commit }) { //异步的action
+            setInterval(() => {
+                commit('increment')
+            }, 2000);
+        }
+
+    }
+})
+export default store //用export default 封装代码，让外部可以引用
