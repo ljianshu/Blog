644323d6050815c1345e22079228e38afec10cb3
人心思动 <lyy15880982687@gmail.com>
2018-10-11 14:47:48 +0800

    Add files via upload

---
+++ Web Workers/worker.js
@@ -0,0 +1,15 @@
+function fibonacci(n) {
+  return n<=2 ? 1 : fibonacci(n-1) + fibonacci(n-2)  //递归调用
+}
+
+console.log(this)
+this.onmessage = function (event) {
+  var number = event.data
+  console.log('分线程接收到主线程发送的数据: '+number)
+  //计算
+  var result = fibonacci(number)
+  postMessage(result)
+  console.log('分线程向主线程返回数据: '+result)
+  // alert(result)  alert是window的方法, 在分线程不能调用
+  // 分线程中的全局对象不再是window, 所以在分线程中不可能更新界面
+}
