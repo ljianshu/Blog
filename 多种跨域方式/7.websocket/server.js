6a93768e1f10bb9253e5c9742a781d9f73bc24f9
浪里行舟 <lyy15880982687@gmail.com>
2019-01-16 00:12:43 +0800

    Add files via upload

---
+++ 多种跨域方式/7.websocket/server.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+let WebSocket = require('ws');
+let wss = new WebSocket.Server({port:3000});
+wss.on('connection',function(ws) {
+  ws.on('message', function (data) {
+    console.log(data);
+    ws.send('我不爱你')
+  });
+})
+
+
+
