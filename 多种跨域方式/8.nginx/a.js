2715671f27c2dd53e7cb071e4747c60b044ebfd8
浪里行舟 <lyy15880982687@gmail.com>
2019-01-18 00:37:07 +0800

    Update a.js

--- 多种跨域方式/8.nginx/a.js
+++ 多种跨域方式/8.nginx/a.js
@@ -1,4 +1,18 @@
-let express = require('express');
-let app = express();
-app.use(express.static(__dirname));
-app.listen(3000);
+var http = require('http');
+var server = http.createServer();
+var qs = require('querystring');
+
+server.on('request', function(req, res) {
+    var params = qs.parse(req.url.substring(2));
+
+    // 向前台写cookie
+    res.writeHead(200, {
+        'Set-Cookie': 'l=a123456;Path=/;Domain=www.domain2.com;HttpOnly'   // HttpOnly:脚本无法读取
+    });
+
+    res.write(JSON.stringify(params));
+    res.end();
+});
+
+server.listen('8080');
+console.log('Server is running at port 8080...');
