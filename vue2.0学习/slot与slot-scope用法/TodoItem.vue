3e4fe79d10e4ae8182b73741b993ad0665380398
浪里行舟 <lyy15880982687@gmail.com>
2019-05-26 18:02:56 +0800

    Add files via upload

---
+++ vue2.0学习/slot与slot-scope用法/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <li class="item">
+      <input v-model="checked" type="checkbox" />
+      <slot name="item" :checked="checked"></slot>
+    </li>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      checked: false
+    };
+  }
+};
+</script>
+<style scoped>
+.item {
+  color: red;
+}
+</style>
---
+++ vue2.0学习/slot与slot-scope用法/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="toList">
+    <input v-model="info" type="text" /> <button @click="addItem">添加</button>
+    <ul>
+      <TodoItem v-for="(item, index) in listData" :key="index">
+        <template v-slot:item="itemProps">
+          <span
+            :style="{
+              fontSize: '20px',
+              color: itemProps.checked ? 'yellow' : 'blue'
+            }"
+            >{{ item }}</span
+          >
+        </template>
+      </TodoItem>
+    </ul>
+  </div>
+</template>
+<script>
+import TodoItem from "./TodoItem";
+export default {
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      info: "",
+      listData: []
+    };
+  },
+  methods: {
+    addItem() {
+      this.listData.push(this.info);
+      this.info = "";
+    }
+  }
+};
+</script>
+<style scoped>
+.toList {
+  margin: 20px;
+}
+</style>
