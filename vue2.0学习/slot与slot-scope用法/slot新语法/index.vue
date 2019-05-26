c417f63de0ab36a7cccf37642a6ea05ed555e510
浪里行舟 <lyy15880982687@gmail.com>
2019-05-26 21:00:16 +0800

    Add files via upload

---
+++ vue2.0学习/slot与slot-scope用法/slot新语法/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="helloSlot">
+    <h2>2.6 新语法</h2>
+    <SlotDemo>
+      <p>默认插槽：default slot</p>
+      <template v-slot:title>
+        <p>具名插槽：title slot1</p>
+        <p>具名插槽：title slot2</p>
+      </template>
+      <template v-slot:item="props">
+        <p>作用域插槽：item slot-scope {{ props }}</p>
+      </template>
+    </SlotDemo>
+  </div>
+</template>
+<script>
+import Slot from "./slot";
+export default {
+  components: {
+    SlotDemo: Slot
+  }
+};
+</script>
+<style>
+.helloSlot {
+  margin: 20px;
+}
+</style>
---
+++ vue2.0学习/slot与slot-scope用法/slot新语法/slot.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot />
+    <slot name="title" />
+    <slot name="item" :propData="propData" />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      propData: {
+        value: "浪里行舟"
+      }
+    };
+  }
+};
+</script>
