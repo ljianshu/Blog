3392e1372576b04e084b21e9a8d12ea44db5ea4a
浪里行舟 <lyy15880982687@gmail.com>
2019-05-26 17:57:39 +0800

    Add files via upload

---
+++ vue2.0学习/如何对prop双向绑定/demo.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="show" class="border">
+    <div>子组件msg：{{ msg }}</div>
+    <div>子组件数组：{{ arr }}</div>
+    <button @click="closeModel">关闭model框</button>
+    <button @click="$emit('update:msg', '浪里行舟')">
+      改变文字
+    </button>
+    <button @click="arr.push('前端工匠')">改变数组</button>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    msg: {
+      type: String
+    },
+    show: {
+      type: Boolean
+    },
+    arr: {
+      type: Array
+    }
+  },
+  methods: {
+    closeModel() {
+      this.$emit("update:show", false);
+    }
+  }
+};
+</script>
+<style scoped>
+.border {
+  border: 1px solid red;
+  margin-top: 10px;
+  padding: 10px;
+}
+</style>
---
+++ vue2.0学习/如何对prop双向绑定/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <div>
+      <input v-model.lazy="value" type="text" />
+      <span style="font-size:20px;margin-left:10px;">{{ value }}</span>
+      <p style="margin-top:10px;">父组件msg：{{ msg }}</p>
+      <p>父组件数组：{{ arr }}</p>
+    </div>
+    <button @click="show = true">打开model框</button>
+    <br />
+    <demo :show.sync="show" :msg.sync="msg" :arr="arr"></demo>
+  </div>
+</template>
+
+<script>
+import Demo from "./demo.vue";
+export default {
+  name: "Hello",
+  components: {
+    Demo
+  },
+  data() {
+    return {
+      value: "",
+      show: false,
+      msg: "模拟一个model框",
+      arr: [1, 2, 3]
+    };
+  }
+};
+</script>
+<style>
+.hello {
+  margin: 20px;
+}
+</style>
