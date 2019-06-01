0d496dea93b174a72347539c286a7a637376ec55
浪里行舟 <lyy15880982687@gmail.com>
2019-06-01 17:45:45 +0800

    Update index.vue

--- vue2.0学习/vue组件间通信/props传递一个函数/index.vue
+++ vue2.0学习/vue组件间通信/props传递一个函数/index.vue
@@ -4,7 +4,7 @@
       name="Hello Vue！"
       :type="type"
       :is-visible="false"
-      :on-change="handlePropChange"
+      :on-change="handlePropChange" // 通过一般属性实现父向子通信；通过函数属性实现子向父通信
       title="属性Demo"
       class="test1"
       :class="['test2']"
