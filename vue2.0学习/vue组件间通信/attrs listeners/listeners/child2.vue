66bca9926944929e77e0bf976464cd0344e8b670
浪里行舟 <lyy15880982687@gmail.com>
2019-07-10 23:37:33 +0800

    Update child2.vue

--- vue2.0学习/vue组件间通信/attrs listeners/listeners/child2.vue
+++ vue2.0学习/vue组件间通信/attrs listeners/listeners/child2.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="hello">
     <p @click="$listeners.changeData('change')">孙子组件</p>
+    <p @click="$listeners.another()">触发祖组件的another事件</p>
+    <p @click="$listeners.other()">触发祖组件的other事件</p>
   </div>
 </template>
 <script>
 export default {
   created() {
-    console.log("child2", this.$listeners);
+    console.log("child2", this.$listeners); // {changeData: ƒ, another: ƒ, other: ƒ}
   }
 };
 </script>
