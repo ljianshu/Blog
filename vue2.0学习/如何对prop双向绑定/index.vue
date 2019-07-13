2177eaf000b9238853ad2d148c4d3e2910c03744
浪里行舟 <lyy15880982687@gmail.com>
2019-07-13 16:57:33 +0800

    Update index.vue

--- vue2.0学习/如何对prop双向绑定/index.vue
+++ vue2.0学习/如何对prop双向绑定/index.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="hello">
     <div>
-      <input v-model.lazy="value" type="text" />
-      <span style="font-size:20px;margin-left:10px;">{{ value }}</span>
-      <p style="margin-top:10px;">父组件msg：{{ msg }}</p>
+      <p>父组件msg：{{ msg }}</p>
       <p>父组件数组：{{ arr }}</p>
+      <p>父组件对象：{{ obj.name }}</p>
     </div>
     <button @click="show = true">打开model框</button>
     <br />
-    <demo :show.sync="show" :msg.sync="msg" :arr="arr"></demo>
+    <demo :show.sync="show" :msg.sync="msg" :arr="arr" :obj="obj"></demo>
   </div>
 </template>
 
@@ -21,16 +20,13 @@ export default {
   },
   data() {
     return {
-      value: "",
       show: false,
       msg: "模拟一个model框",
-      arr: [1, 2, 3]
+      arr: [1, 2, 3],
+      obj: {
+        name: "厦门"
+      }
     };
   }
 };
 </script>
-<style>
-.hello {
-  margin: 20px;
-}
-</style>
