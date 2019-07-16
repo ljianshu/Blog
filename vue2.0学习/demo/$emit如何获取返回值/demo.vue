fd9a2297439e4855e7f266889c6d245ba2b795f6
浪里行舟 <lyy15880982687@gmail.com>
2019-07-16 23:51:29 +0800

    Add files via upload

---
+++ vue2.0学习/demo/$emit如何获取返回值/demo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        name: {{ name || "--" }}
+        <br />
+        <input :value="name" @change="handleChange" />
+        <br />
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        name: String
+    },
+    methods: {
+        handleChange(e) {
+            const res = this.$emit("change", e.target.value, val => {
+                console.log("父组件callback传递过来", val);
+            });
+            console.log(res, res === this); // 实例  true
+        }
+    }
+};
+</script>
---
+++ vue2.0学习/demo/$emit如何获取返回值/index.vue
@@ -0,0 +1,35 @@
+// this.$emit的返回值是什么？ this // 如果需要返回值可以使用回调参数
+<template>
+  <div class="hello">
+    <div>
+      <Demo :name="name" @change="handleEventChange" />
+    </div>
+    <br />
+  </div>
+</template>
+<script>
+import Demo from "./demo.vue";
+export default {
+  components: {
+    Demo
+  },
+  data() {
+    return {
+      name: ""
+    };
+  },
+  methods: {
+    handleEventChange(val, callback) {
+      console.log("子组件传递过来", val, callback);
+      this.name = val;
+      callback("helloWorld");
+      return "hello";
+    }
+  }
+};
+</script>
+<style>
+.hello {
+  margin: 30px;
+}
+</style>
