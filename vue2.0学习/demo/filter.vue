36cf896d7e9f4a5c800bb6b7a7b30310317143c5
浪里行舟 <lyy15880982687@gmail.com>
2019-07-17 23:57:48 +0800

    Add files via upload

---
+++ vue2.0学习/demo/filter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <div>
+      <h2>显示格式化的日期时间</h2>
+      <p>{{ date }}</p>
+      <p>{{ date | filterDate }}</p>
+      <p>年月日: {{ date | filterDate("YYYY-MM-DD") }}</p>
+    </div>
+  </div>
+</template>
+<script>
+import moment from "moment";
+export default {
+  filters: {
+    filterDate(value, format = "YYYY-MM-DD HH:mm:ss") {
+      return moment(value).format(format);
+    }
+  },
+  data() {
+    return {
+      date: new Date()
+    };
+  }
+};
+</script>
+<style>
+.hello {
+  margin: 30px;
+}
+</style>
